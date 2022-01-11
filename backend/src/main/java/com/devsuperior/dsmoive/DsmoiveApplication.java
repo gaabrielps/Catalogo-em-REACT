1d9d62d79c8828f1a0e1077821f5262ec4f2d49a
Gabriel Lopes <araujolopesgabriel@gmail.com>
2022-01-11 08:38:35 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmoive/DsmoiveApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoiveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmoive/DsmoiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
